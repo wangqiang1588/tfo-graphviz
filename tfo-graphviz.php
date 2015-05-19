abf803b83a0717748a239ee05c76c7bb4ab87c43
chrisy <chrisy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-19 23:11:27 +0000

    version 1.9
    git-svn-id: https://plugins.svn.wordpress.org/tfo-graphviz/trunk@1163724 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tfo-graphviz.php
+++ tfo-graphviz.php
@@ -11,7 +11,7 @@
 Plugin Name: TFO Graphviz
 Plugin URI: http://blog.flirble.org/projects/graphviz
 Description: Converts inline DOT code into an image, with optional image map, using Graphviz.
-Version: 1.8
+Version: 1.9
 Author: Chris Luke
 Author URI: http://blog.flirble.org/
 Copyright: Chris Luke
